b04bf4ce9ddc43224b3a5eeab8794a3898e65c20
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-12-11 21:40:38 -0300

    Update 1

---
+++ Projeto_Banco/src/Programa/pessoa.java
@@ -0,0 +1,30 @@
+package Programa;
+
+public class pessoa {
+	
+	private static int counter =1;
+	
+	private String nome;
+	private String CPF;
+	private String email;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCPF() {
+		return CPF;
+	}
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
